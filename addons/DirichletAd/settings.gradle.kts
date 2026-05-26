0bdf4ed7071c3b994d319d576f47cbb55adc572a
pipos52 <1460691428@qq.com>
2026-05-26 15:07:22 +0800

    Add addons/DirichletAd/settings.gradle.kts

---
+++ addons/DirichletAd/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        flatDir {
+            dirs("src/main/libs")
+        }
+    }
+}
+
+rootProject.name = "DirichletAd"
